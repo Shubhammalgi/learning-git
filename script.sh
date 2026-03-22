a6f74a6d1c812b00c590db8f3ad5c561114f647f
shubham malgi <ssmalgi@gmail.com>
2026-03-22 12:47:13 +0000

    add script excution

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "🚀 DevOps Script Running"
+echo "Current user:"
+whoami
+
+echo "Current directory:"
+pwd
+
+echo "Files in directory:"
+ls -la
+
+echo "System info:"
+uname -a
+
+echo "Date:"
+date
